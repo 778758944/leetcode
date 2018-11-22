21e3d2353ea334b3f52a9df4e4b07161916313e0
WENTAO XING <a778758944@gmail.com>
2018-11-22 09:09:15 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by WENTAO XING on 2018/11/22.
+//  Copyright © 2018 WENTAO XING. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
